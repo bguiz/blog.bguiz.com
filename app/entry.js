659c6b99aebc1df8c834b8eeb54393153a95a071
Brendan Graetz <github@bguiz.com>
2016-04-14 18:42:57 +0800

    =BG= move from own-rolled GA solution to recat-ga      because this sends page views upon react-router state navigation
    - also lists in the markdown component get bullet points
      (originally removed by reset.css)

--- app/entry.js
+++ app/entry.js
@@ -2,13 +2,31 @@
 
 const reactpubEntry = require('reactpub/entry');
 const reactHelmet = require('react-helmet');
+const reactGa = require('react-ga');
 
 const routes = require('./routes.jsx');
 const data = require('../data/data.js');
+const config = require('./config.js');
+
+function routerOnUpdate() {
+  let url = `${config.baseUrl}${this.state.location.pathname}`;
+  console.log('routerOnUpdate', url, this, arguments);
+  reactGa.pageview(url);
+}
+let reactGaOptions = {
+  id: config.googleAnalyticsId,
+  options: {
+    debug: (typeof document !== 'undefined'),
+    gaOptions: {},
+  },
+};
 
 let renderServer = reactpubEntry({
   reactOnClient: true,
   reactHelmet,
+  reactGa,
+  reactGaOptions,
+  routerOnUpdate,
   routes,
   routeMetadata: data,
   additionalAssets: {

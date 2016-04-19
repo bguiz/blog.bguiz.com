c9cefa2bfb2977f010923c32ef1380e3bba3d36c
Brendan Graetz <github@bguiz.com>
2016-04-19 16:05:02 +0800

    =BG= theme changed settings API - bguiz/reactpub-blog.basic-theme#2f077c59

--- app/entry.js
+++ app/entry.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const reactpubSettings = require('reactpub/settings');
 const theme = require('reactpub-blog-basic-theme');
 
-const data = require('../data/data.js');
-const config = require('./config.js');
-reactpubSettings.set({
-  data,
-  config,
+theme.settings.set({
+  data: require('../data/data.js'),
+  config: require('./config.js'),
 });
+
 module.exports = theme.render();

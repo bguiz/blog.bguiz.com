79c1d1246736d3c28334ce9bbdd6f91b2598c287
Brendan Graetz <github@bguiz.com>
2016-04-25 12:50:26 +0800

    =BG= got-or-not invoked via testdocs, called by newer version of autodocs

--- app/got-or-not.js
+++ app/got-or-not.js
@@ -4,6 +4,12 @@ const gotOrNot = require('got-or-not');
 
 const data = require('../data/data.js');
 
+if (!data.props.aliases ||
+    Object.keys(data.props.aliases).length < 1) {
+  // We know that this blog should have some aliases
+  throw new Error('Expected at least 1 alias.');
+}
+
 let files = data.routes.map((file) => {
   if (!file.match(/\.[A-Za-z0-9]+$/)) {
     // If there is no alphanumeric extension,
